1e17d9e0081d3990037becba943d6f8fc2a0b232
a2236sh <a2236+1@aiit.ac.jp>
2022-08-02 21:51:06 +0900

    fix test.sh execute bash

--- test.sh
+++ test.sh
@@ -14,19 +14,19 @@ is_expected () {
 
 # 正常系テスト
 echo "2つの引数を渡すと最大公約数が返ること(第二引数に大きな値)"
-result=`sh gcd.sh 5 25`
+result=`bash gcd.sh 5 25`
 expected=5
 
 is_expected $result $expected
 
 echo "2つの引数を渡すと最大公約数が返ること(第一引数に大きな値)"
-result=`sh gcd.sh 1708 36`
+result=`bash gcd.sh 1708 36`
 expected=4
 
 is_expected $result $expected
 
 echo "2つの引数を渡すと最大公約数が返ること(同値を渡す)"
-result=`sh gcd.sh 15 15`
+result=`bash gcd.sh 15 15`
 expected=15
 
 is_expected $result $expected
@@ -34,49 +34,49 @@ is_expected $result $expected
 
 # 異常系テスト
 echo "0で始まる数字を引数で渡すとエラーが返ること"
-result=`sh gcd.sh 005 025`
+result=`bash gcd.sh 005 025`
 expected='error:0埋めの数字はセットできません。引数には自然数を設定してください'
 
 is_expected $result $expected
 
 
 echo "引数の数が不足しているとエラーを返すこと"
-result=`sh gcd.sh 5`
+result=`bash gcd.sh 5`
 expected='error:引数の数が正しくありません。2つの自然数を引数に設定してください'
 
 is_expected $result $expected
 
 
 echo "引数の数が多いとエラーを返すこと"
-result=`sh gcd.sh 5 25 125`
+result=`bash gcd.sh 5 25 125`
 expected='error:引数の数が正しくありません。2つの自然数を引数に設定してください'
 
 is_expected $result $expected
 
 
 echo "引数に小数点を渡すとエラーを返すこと(第一引数)"
-result=`sh gcd.sh 1.5 25`
+result=`bash gcd.sh 1.5 25`
 expected='error:第一引数に利用できない値がセットされています。引数には自然数を設定してください'
 
 is_expected $result $expected
 
 
 echo "引数に小数点を渡すとエラーを返すこと(第二引数)"
-result=`sh gcd.sh 25 1.7`
+result=`bash gcd.sh 25 1.7`
 expected='error:第二引数に利用できない値がセットされています。引数には自然数を設定してください'
 
 is_expected $result $expected
 
 
 echo "引数に数字以外を渡すとエラーを返すこと(第一引数)"
-result=`sh gcd.sh 'hoge' 16`
+result=`bash gcd.sh 'hoge' 16`
 expected='error:第一引数に利用できない値がセットされています。引数には自然数を設定してください'
 
 is_expected $result $expected
 
 
 echo "引数に数字以外を渡すとエラーを返すこと(第二引数)"
-result=`sh gcd.sh 12 'fuga'`
+result=`bash gcd.sh 12 'fuga'`
 expected='error:第二引数に利用できない値がセットされています。引数には自然数を設定してください'
 
 is_expected $result $expected
